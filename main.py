fe884e76af3acf4bad7065d34bec19c4f68f65db
PizzaAlSalamePiccante <147008087+PizzaAlSalamePiccante@users.noreply.github.com>
2023-11-25 22:08:29 +1000

    Небольшая палировка 0.1

--- main.py
+++ main.py
@@ -28,41 +28,40 @@ def look_option():
     for i, row in enumerate(inventory):
         if index in row:
             inInv = row.index(index)
-            print(f'Найдено на позиции - {i}')
             print(f'Название товара: {inventory[i][0]}', end=' | ')
             print(f'Описание товара: {inventory[i][1]}', end=' | ')
             print(f'Цена товара: {inventory[i][2]}', end=' | ')
             print(f'Количество товара: {inventory[i][3]}', end=' | ')
-            print(f'Категория товара: {inventory[i][4]}')
+            print(f'Категория товара: {inventory[i][4]}', end=' | ')
+            print(f'Позиция в реестре: {i}')
             break
 
         else:
-            print(f'Не найдено на позиции - {i}')
+            print('Поиск..')
 def edit_option():
     index = input('Введите название: ')
     for i, row in enumerate(inventory):
         if index in row:
             inInv = row.index(index)
-            print(f'Найдено на позиции - {i}')
             while True:
-                doEdit = input('Что изменить (Название, Описание, Цена, Количество, Категория): ')
+                doEdit = input('Что изменить (Название, Описание, Цена, Количество, Категория): ').lower()
                 if doEdit.lower() == 'название':
                     inventory[i][0] = input('Введите новое название: ')
                     break
                 elif doEdit.lower() == 'описание':
-                    inventory[i][0] = input('Введите новое описание: ')
+                    inventory[i][1] = input('Введите новое описание: ')
                     break
                 elif doEdit.lower() == 'цена':
-                    inventory[i][0] = input('Введите новую цену: ')
+                    inventory[i][2] = input('Введите новую цену: ')
                     break
                 elif doEdit.lower() == 'количество':
-                    inventory[i][0] = input('Введите новое количесво: ')
+                    inventory[i][3] = input('Введите новое количесво: ')
                     break
                 elif doEdit.lower() == 'категория':
-                    inventory[i][0] = input('Введите новую категорию: ')
+                    inventory[i][4] = input('Введите новую категорию: ')
                     break
                 else:
-                    print('Ты шо головой ударился, написанно на исконно русском языке что можно выбрать балбес')
+                    print('Ты шо головой ударился, написанно на исконно русском языке что можно выбрать, балбес')
                     continue
             break
         else:
@@ -88,25 +87,40 @@ def delete_option():
 
         else:
             print(f'Не найдено на позиции - {i}')
+def look_on_all_array():
+    for i, row in enumerate(inventory):
+        print(f'Название: {inventory[i][0]} | Описание: {inventory[i][1]} | Цена: {inventory[i][2]}  | Количество: {inventory[i][3]} | Категория: {inventory[i][4]}')
+
 while True:
     invlen = len(inventory) - 1
-    print('Колво - ' + str(invlen))
-    do = input('Что делать: ')
-    if do.lower() == '1':
+    do = input('Что делать? (Создать | Просмотреть | Изменить | Удалить | Выйти) : ')
+    if do.lower() == 'Создать'.lower():
         input_and_create()
-    elif do.lower() == '2':
-        look_option()
-    elif do.lower() == '3':
+    elif do.lower() == 'Просмотреть'.lower():
+        print(f'Количество значений в базе - {invlen}')
+        while True:
+            oneOrAll = input('Все варианты или определенный (Все | Один): ')
+            if oneOrAll.lower() == 'все':
+                look_on_all_array()
+                break
+            elif oneOrAll.lower() == 'один':
+                look_option()
+                break
+            else:
+                print(f'Я все понимаю но такого сделать не могу, хоть убей - {oneOrAll}')
+    elif do.lower() == 'Изменить'.lower():
         edit_option()
-    elif do.lower() == '4':
+    elif do.lower() == 'Удалить'.lower():
         delete_option()
-    elif do.lower() == '5':
-        loop = input('Продожить? y/n   ')
-        if loop == 'y':
+    elif do.lower() == 'Выйти'.lower():
+        loop = input('Выйти? y/n   ')
+        if loop == 'n':
             continue
         else:
             save_inventory()
             exit()
+    else:
+        print(f'Ну я все понимаю, но такого мы не умеем - {do}')
 
 
 
